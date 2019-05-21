fc7136815e22d85b881027f977117659fe0fc52c
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-20 19:04:02 -0700

    Added database and GET endpoint

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 
-const server = express();
+const Accounts = require('./data/accounts-model.js');
 
-// your code here
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send(`<h2>My first database!</h2>`)
+});
+
+server.get('/api/accounts', async (req, res) => {
+    try {
+        const accounts = await Accounts.find();
+        res.status(200).json(accounts);
+      } catch (error) {
+        res.status(500).json({
+          message: 'Error retrieving the accounts',
+        });
+      }
+});
 
 module.exports = server;
