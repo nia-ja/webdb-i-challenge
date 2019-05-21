8ceee568128ef9d0f44b78b3abf003b9ff8f32cf
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-20 20:52:15 -0700

    Added GET by ID endpoint

--- server.js
+++ server.js
@@ -17,8 +17,22 @@ server.get('/api/accounts', async (req, res) => {
       } catch (error) {
         res.status(500).json({
           message: 'Error retrieving the accounts',
+          error: error
         });
       }
 });
+
+server.get('/api/accounts/:id', async (req, res) => {
+    try {
+        const account = await Accounts.findById(req.params.id);
+        if(account) {
+            res.status(200).json(account);
+        } else {
+            res.status(404).json({ message: "The account with the specified ID does not exist." })
+        }
+    } catch (error) {
+        res.status(500).json({ message: "The account information could not be retrieved.", error: error })
+    }
+})
 
 module.exports = server;
